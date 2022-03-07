920161b8bafbe12df0a117ec4973119f39cc84a6
hustone <jheymonet@gmail.com>
2022-03-07 18:07:21 +0100

    adding a print function to clarify the np.mean function

--- tuto_numpy.py
+++ tuto_numpy.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-random_values = np.random.randint(0, 10, 100)
+random_values = np.random.randint(0, 10**10, 100)
 expectation = np.mean(random_values)
 std = np.std(random_values)
 
 if __name__ == "__main__":
-    print(expectation)
+    print("the mathematical expectation of a random variables is {}".format(expectation))
     print(std)
